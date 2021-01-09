7f46aa113f9449d2364079129172b4d360a7fe48
Lzhcapple <zhangchaopear@163.com>
2021-01-09 13:58:14 +0800

    Update A.java
    gethub

--- src/main/java/enjoy/git/A.java
+++ src/main/java/enjoy/git/A.java
@@ -2,6 +2,6 @@ package enjoy.git;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("v0.10--master update git merge Lzhcapple 合并分支 -测试https上传");
+        System.out.println("v0.10--master update git merge Lzhcapple 合并分支 -测试github");
     }
 }
