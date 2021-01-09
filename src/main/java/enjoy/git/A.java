57b92838ff0d574c245c03dc35371e0a86e7669b
Lzhcapple <zhangchaopear@163.com>
2021-01-09 13:46:42 +0800

    v0.11-ssh

--- src/main/java/enjoy/git/A.java
+++ src/main/java/enjoy/git/A.java
@@ -2,6 +2,6 @@ package enjoy.git;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("v0.10--master update git merge Lzhcapple 合并分支");
+        System.out.println("v0.10--master update git merge Lzhcapple 合并分支 -ssh更改上传");
     }
 }
